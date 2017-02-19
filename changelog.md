02abb5da7b6a310fea5748d20482e13b9a0f0054
Brendan Tobolaski <brendan@tobolaski.com>
2017-02-19 11:04:52 -0600

    Update dependencies

---
+++ changelog.md
@@ -0,0 +1,11 @@
+# Change Log
+All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
+
+## [Unreleased]
+### Changed
+
+## 0.3.3 - 2017-02-19
+### Changed
+- Updated dependencies
+
+[Unreleased]: https://github.com/your-name/muninn/compare/0.3.3...HEAD
--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://gitlab.com/robin.heggelund/hugsql-postgres-async"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
-                 [com.layerware/hugsql-adapter "0.3.1"]
-                 [alaisi/postgres.async "0.6.0"]])
+                 [com.layerware/hugsql-adapter "0.4.7"]
+                 [alaisi/postgres.async "0.8.0"]])
